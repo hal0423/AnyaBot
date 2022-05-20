628c7e3257a476118d45227e678ead983304d3fb
hal0423 <theha2349@gmail.com>
2022-05-20 12:10:47 -0400

    first commit

---
+++ src/main/java/com/itsamekawa/anyabot/AnyaBotApplication.java
@@ -0,0 +1,13 @@
+package com.itsamekawa.anyabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyaBotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/itsamekawa/anyabot/config/BotConfiguration.java
@@ -0,0 +1,45 @@
+package com.itsamekawa.anyabot.config;
+
+import com.itsamekawa.anyabot.util.EventListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class BotConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BotConfiguration.class);
+    @Value("${token}")
+    private String token;
+
+    @Bean
+    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
+        GatewayDiscordClient client = null;
+
+        try {
+            client = DiscordClientBuilder.create(token)
+                    .build()
+                    .login()
+                    .block();
+
+            for(EventListener<T> listener : eventListeners) {
+                assert client != null;
+                client.on(listener.getEventType())
+                        .flatMap(listener::execute)
+                        .onErrorResume(listener::handleError)
+                        .subscribe();
+            }
+        }
+        catch (Exception e) {
+            LOGGER.error("invalid token", e);
+        }
+
+        return client;
+    }
+}
